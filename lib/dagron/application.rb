3a39283f535b45713ac9e09f2ecd0d9152edd935
Jeremy Stephens <viking@pillageandplunder.net>
2013-07-09 20:12:13 -0500

    Add /maps route and view

--- lib/dagron/application.rb
+++ lib/dagron/application.rb
@@ -6,5 +6,27 @@ module Dagron
     get "/" do
       erb :index
     end
+
+    get "/maps" do
+      @maps = Map.all
+      erb :'maps/index'
+    end
+
+    post "/maps" do
+      name = params[:name]
+      filename = data = nil
+      if params[:data].is_a?(Hash)
+        filename = params[:data][:filename]
+        data = params[:data][:tempfile].read
+      end
+      map = Map.new({
+        :name => params[:name], :filename => filename,
+        :data => data
+      })
+      if map.valid?
+        map.save
+        redirect '/maps'
+      end
+    end
   end
 end
--- test/unit/test_application.rb
+++ test/unit/test_application.rb
@@ -11,6 +11,35 @@ class TestApplication < Test::Unit::TestCase
   test "index" do
     get '/'
     assert last_response.ok?
-    assert_match "sup?", last_response.body
+  end
+
+  test "maps index" do
+    map = stub('map', :id => 1, :name => "Foo")
+    Dagron::Map.expects(:all).returns([map])
+    get '/maps'
+    assert last_response.ok?
+  end
+
+  test "create map" do
+    file = Tempfile.new('dagron')
+    file.puts("<map></map>")
+    file.flush
+    file.rewind
+    begin
+      map = mock('map', :valid? => true, :save => true)
+      Dagron::Map.expects(:new).with({
+        :name => 'foo',
+        :filename => File.basename(file.path),
+        :data => "<map></map>\n"
+      }).returns(map)
+      post '/maps', {
+        :name => 'foo',
+        :data => Rack::Test::UploadedFile.new(file, "text/xml")
+      }
+      assert last_response.redirect?
+      assert_equal 'http://example.org/maps', last_response['location']
+    ensure
+      file.unlink
+    end
   end
 end
