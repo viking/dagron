08fa6eb77d00307e277369462fe8667a2f7178c5
Jeremy Stephens <viking@pillageandplunder.net>
2013-08-10 19:56:33 -0500

    Set viewport value defaults

--- db/migrate/001_create_maps.rb
+++ db/migrate/001_create_maps.rb
@@ -3,10 +3,10 @@ Sequel.migration do
     create_table :maps do
       primary_key :id
       String :name
-      Fixnum :viewport_x
-      Fixnum :viewport_y
-      Fixnum :viewport_w
-      Fixnum :viewport_h
+      Fixnum :viewport_x, :default => 0
+      Fixnum :viewport_y, :default => 0
+      Fixnum :viewport_w, :default => 0
+      Fixnum :viewport_h, :default => 0
       DateTime :created_at
       DateTime :updated_at
     end
